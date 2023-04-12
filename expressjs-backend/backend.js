52a9bca61e7d6b8bb5fad7ca6dbd07c23285dbb0
Tamim Fatahi <tamimfatahi@gmail.com>
2023-04-12 09:56:13 -0700

    Adding the ability to add users temporarily in memory

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -62,6 +62,16 @@ app.get('/users/:id', (req, res) => {
     }
 });
 
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
 function findUserById(id) {
     return users['users_list'].find( (user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
