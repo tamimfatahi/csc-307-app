11a516eb721f8ccc80edea7dbb4b555cc0f0eb68
Tamim Fatahi <tamimfatahi@gmail.com>
2023-04-14 03:51:56 -0700

    Added Axios into frontend code and CORS

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -29,10 +29,12 @@ const users = {
     ]
  }
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
