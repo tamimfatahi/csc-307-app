1f856f66706f003655440e5d4756de2ecff6b00a
Tamim Fatahi <tamimfatahi@gmail.com>
2023-04-14 03:54:59 -0700

    added a submit button the ability to add users using POST

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -30,6 +30,24 @@ function MyApp() {
       }
    }
 
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:8000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+  }
+
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 200)
+       setCharacters([...characters, person] );
+    });
+  }
+
   useEffect(() => {
     fetchAll().then( result => {
         if (result)
